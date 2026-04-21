1f1bb95103317b6ab566bb842bc395d8e0067cf7
bautigelp06 <bautigelp06@gmail.com>
2026-04-21 14:51:37 -0300

    Ejercicio 1

---
+++ clase 3-javascript/ejercicio 1/app.js
@@ -0,0 +1,13 @@
+const nombre = "Bautista Sciaroni";
+const edad = 19;
+const materia = "Desarrollo de Software";
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+let contador = 0;
+
+contador++;
+contador++;
+contador++;
+
+console.log("Valor final del contador:", contador);
