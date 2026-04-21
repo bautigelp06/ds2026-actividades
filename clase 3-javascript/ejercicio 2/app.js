79f125069e1f0129f9c6f28358b5f783a8198753
bautigelp06 <bautigelp06@gmail.com>
2026-04-21 15:04:51 -0300

    Ejercicio 2

--- clase 3-javascript/ejercicio 2/app.js
+++ clase 3-javascript/ejercicio 2/app.js
@@ -1,13 +1,34 @@
-const nombre = "Bautista Sciaroni";
-const edad = 19;
-const materia = "Desarrollo de Software";
+function clasificarNota(nota) {
+    if (nota < 4) {
+        return "Desaprobado";
+    } else if (nota >= 4 && nota <= 7) {
+        return "Aprobado";
+    } else {
+        return "Promocionado";
+    }
+}
 
-console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
 
-let contador = 0;
+console.log("Nota 3:", clasificarNota(3)); 
+console.log("Nota 6:", clasificarNota(6)); 
+console.log("Nota 9:", clasificarNota(9));  
 
-contador++;
-contador++;
-contador++;
 
-console.log("Valor final del contador:", contador);
+
+
+function queDiaEs(numero) {
+    switch (numero) {
+        case 1: return "Lunes";
+        case 2: return "Martes";
+        case 3: return "Miércoles";
+        case 4: return "Jueves";
+        case 5: return "Viernes";
+        case 6: return "Sábado (Es fin de semana)";
+        case 7: return "Domingo (Es fin de semana)";
+        default: return "Número inválido. Ingresá del 1 al 7 para devolver un dia de la semana.";
+    }
+}
+
+console.log("Día 2:", queDiaEs(2));
+console.log("Día 6:", queDiaEs(6)); 
+console.log("Día 10:", queDiaEs(10)); 
